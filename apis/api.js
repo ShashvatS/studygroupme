d640513b126ea1c1d008ee0cc1f751cfcb6ecbb1
Shashvat Srivastava <shashvat.sri@gmail.com>
2019-10-20 03:06:01 -0400

    minor debugging

--- apis/api.js
+++ apis/api.js
@@ -3,8 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var router = express.Router();
 router.post('/register', function (req, res) {
-    console.log(req.body);
-    res.json(req.body);
+    console.log(req);
+    if (req.body) {
+        res.json(req.body);
+    }
+    else {
+        res.json({});
+    }
 });
 exports.default = router;
 //# sourceMappingURL=api.js.map
--- apis/api.ts
+++ apis/api.ts
@@ -3,8 +3,14 @@ import express = require('express');
 const router = express.Router();
 
 router.post('/register', (req: express.Request, res: express.Response) => {
-    console.log(req.body);
-    res.json(req.body);
+    console.log(req);
+    
+    if (req.body) {
+        res.json(req.body);
+    }
+    else {
+        res.json({});
+    }    
 });
 
 export default router;
